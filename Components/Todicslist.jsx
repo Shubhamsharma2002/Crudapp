2a559b637185b32b59e63bc9ac2b132e34bac02b
shubham sharma <shubhamtech004@gmail.com>
2025-04-04 23:16:58 +0530

    css fixing

--- Components/Todicslist.jsx
+++ Components/Todicslist.jsx
@@ -1,11 +1,12 @@
 
 
 import React from "react";
-
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const Topics = ({id,title ,description}) =>{
     return (
-          <div className=" mt-7 ml-2 rounded-lg  w-64 border-2 border-amber-800">
+          <div className=" min-h-10  mt-7 ml-2 rounded-lg  w-64 border-2 border-amber-800">
             <div className=" text-center  bg-amber-800">
                   Task :{id}
                   </div>
@@ -13,9 +14,9 @@ const Topics = ({id,title ,description}) =>{
                     <p>Title:{title}</p>
                     <p>description :{description}</p>
                   </div>
-                  <div className="flex justify-around">
-                     <p>edit</p>
-                     <p>delete</p>
+                  <div className="flex justify-around mt-7 mb-0 ">
+                  <p><MdDelete className="text-red-500 h-5 w-10 cursor-pointer"/></p>
+                  <p><FaEdit  className="text-blue-500 h-5 w-10 cursor-pointer"/></p>  
                   </div>
             </div>
             
