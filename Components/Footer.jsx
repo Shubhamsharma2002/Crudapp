db150ed9f84414765d88b6d1dee4fba710abd0b4
shubham sharma <shubhamtech004@gmail.com>
2025-04-05 00:04:08 +0530

    footer section done

---
+++ Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer  = () =>{
+    return(
+       <div className="flex justify-around bg-amber-500 items-center mt-5 text-white font-bold p-5">
+         <div> All right reserved to the @Shubha Sharma</div>
+         <div >
+           
+            <div  className="flex">
+                <p className="ml-5 cursor-pointer">intta </p>
+                <p className="ml-5 cursor-pointer">gmail</p>
+            </div>
+         </div>
+       </div>
+    )
+}
+
+export default Footer
