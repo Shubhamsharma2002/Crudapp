92defe8225801d3ba9a9e0140a4b8054777b49b9
shubham sharma <shubhamtech004@gmail.com>
2025-04-07 23:29:36 +0530

    code debuging

--- models/addtask.js
+++ models/addtask.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 const topicSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    title: { 
+        type: String,
+       required: true 
+      },
+    description: { 
+      type: String,
+     required: true
+     },
   },
   { timestamps: true }
 );
