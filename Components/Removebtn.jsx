5adcd9fc838e534f769159c2d595ef93b6745c5f
shubham sharma <shubhamtech004@gmail.com>
2025-04-10 01:21:07 +0530

    minimal debuging with the page reload

--- Components/Removebtn.jsx
+++ Components/Removebtn.jsx
@@ -1,18 +1,19 @@
 'use client'
+import { useRouter } from "next/navigation";
 import React from "react";
 import { MdDelete } from "react-icons/md";
 
 const Deletebtn = ({id}) =>{
+    const router = useRouter()
     const handledlete = async() =>{
            const confremed = confirm("are you sure to delete this");
 
            if(confremed){
-         const res = await  fetch(`/api/topics?id=${id}`,{
+           const res = await  fetch(`/api/topics?id=${id}`,{
             method:"DELETE",
            })
             if(res.ok){
-                console.log(res);
-                
+                router.refresh();
             }
         }
 
