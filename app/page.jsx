e5af9b64a436af514786fa12b4a790db84164ae3
shubham sharma <shubhamtech004@gmail.com>
2025-04-09 19:21:40 +0530

    fetching the get api for showing the real data

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,36 @@
-"use client";
+
 import Topics from "@/Components/Todicslist";
-import { useEffect, useState } from "react";
 
 
-const  Home = () =>{
-  const [val,setvalues] = useState([]);
-      const getData = () =>{
-          fetch(`https://jsonplaceholder.typicode.com/todos`)
-          .then((res) => res.json())
-          .then((data)=> setvalues(data.splice(0, 20)))
-          .catch((error)=>console.log(error))
-          
-      }
-        console.log(val);
-        
-          
-      useEffect(()=>{
-          getData();
-      },[])
+const getdata = async()=>{
+  try {
+       const res = await fetch('http://localhost:3000/api/topics',{
+         cache:"no-store"
+       })
+       if(!res.ok){
+         throw new Error("Failed to fetch the data")
+       }
+       const data = await res.json();
+       console.log(data);
+       
+       return data;
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+const  Home = async() =>{
+  const result = await getdata();
+  const val = result?.data || [];
   return (
         
          <div className="flex justify-evenly flex-wrap">
-          {val.map((p) =>(
+          {val.map((p,index) =>(
             <Topics 
-              key={p.id}
-               id={p.id}
+              key={p._id}
+              id={index + 1}
                title={p.title}
+               description={p.description}
             />
          ))}
          </div>
