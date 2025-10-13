2b7645522f68db9cb4974858517e7ec75f29efeb
shubham sharma <shubhamtech004@gmail.com>
2025-10-13 23:37:37 +0530

    line up

--- Components/Removebtn.jsx
+++ Components/Removebtn.jsx
@@ -1,30 +1,30 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { MdDelete } from "react-icons/md";
 
-const Deletebtn = ({id}) =>{
-    const router = useRouter()
-    const handledlete = async() =>{
-           const confremed = confirm("are you sure to delete this");
+const Deletebtn = ({ id }) => {
+  const router = useRouter();
+  const handledlete = async () => {
+    const confremed = confirm("are you sure to delete this");
 
-           if(confremed){
-           const res = await  fetch(`/api/topics?id=${id}`,{
-            method:"DELETE",
-           })
-            if(res.ok){
-                router.refresh();
-            }
-        }
-
+    if (confremed) {
+      const res = await fetch(`/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        router.refresh();
+      }
     }
-    // console.log(id);
-    
-    return(
+  };
+  // console.log(id);
 
-        
-        <MdDelete onClick={()=>handledlete(id)} className="text-red-500 h-5 w-10 cursor-pointer"/>
-    )
-}
+  return (
+    <MdDelete
+      onClick={() => handledlete(id)}
+      className="text-red-500 h-5 w-10 cursor-pointer"
+    />
+  );
+};
 
 export default Deletebtn;
